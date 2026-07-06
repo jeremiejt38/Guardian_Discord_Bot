4eba77a7fe0d56c6983755fdd68c5d1ababdcbcf
jeremiejt38 <jeremiejt38@gmail.com>
2026-07-06 14:14:11 +0200

    cleanup: suppr permissions.js/changelogsPanel.js/statusBotPanel.js/fr.js obsolètes, fix ready.js, fusion fr.json

--- guardian/events/ready.js
+++ guardian/events/ready.js
@@ -7,7 +7,6 @@ const { applyPersistedSlowModeForGuild } = require('../modules/moderation/autoMo
 const { ensureMemberGameInterfaces } = require('../modules/config/settings');
 const { runPassiveScoreRegen } = require('../modules/moderation/behavior');
 const { seedGuildMessages } = require('../modules/initialisation/seeds');
-const { refreshStatusBotPanel } = require('../modules/config/statusBotPanel');
 const logger = require('../modules/logs/logger');
 
 module.exports = {
@@ -22,11 +21,10 @@ module.exports = {
           await createSetupArea(guild);
         }
 
-        await ensureSetupInstallPrompt(guild);
+        await ensureSetupInstallPrompt(guild, { forceCreateIfMissing: true });
 
         await applyPersistedSlowModeForGuild(guild);
         await ensureMemberGameInterfaces(guild);
-        await refreshStatusBotPanel(guild).catch(() => undefined);
         await seedGuildMessages(guild).catch(() => undefined);
       } catch (error) {
         logger.error(`Failed ready setup check for guild ${guild.id}`, error);
