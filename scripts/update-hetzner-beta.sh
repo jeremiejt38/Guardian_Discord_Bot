ce0a8acc6f27009a082b7969057bde846807775a
jeremiejt38 <jeremiejt38@gmail.com>
2026-07-15 05:39:50 +0200

    feat(deploy): add Hetzner update scripts for beta and stable instances

---
+++ scripts/update-hetzner-beta.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+cd /opt/guardian-beta
+git fetch origin
+git reset --hard origin/beta
+
+cd guardian
+npm ci --omit=dev
+node deploy-commands.js --env=beta
+
+if pm2 describe guardian-beta >/dev/null 2>&1; then
+  pm2 restart guardian-beta
+else
+  pm2 start npm --name guardian-beta -- run start:beta
+fi
+
+pm2 save
